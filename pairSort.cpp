b0dc10a818169ec3b06d9bb8205bed022dd4e1b6
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2022-07-31 17:33:31 +0530

    sort pair< ll , ll >

--- pairSort.cpp
+++ pairSort.cpp
@@ -7,3 +7,11 @@ bool cmp(pair<int, string> a, pair<int, string> b) {
         return a.second < b.second;  // dec
     }
 } // inc dec
+
+bool cmp(pair<int, string> a, pair<int, string> b) {
+    if (a.first != b.first) {
+        return a.first < b.first; // dec
+    } else {
+        return a.second > b.second;  // ince
+    }
+} // dec inc 
