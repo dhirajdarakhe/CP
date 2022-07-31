7a1547ae3af5f4fda0ad6baf86dc98b15954bc24
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2022-07-31 17:39:32 +0530

    prime num

---
+++ primeNUM.cpp
@@ -0,0 +1,17 @@
+vector<int> prime(10000000, 1);
+void isPrime()
+{
+
+    int c = 0;
+    for (int p = 2; p * p <= 10000000; p++)
+
+        if (prime[p] == 1)
+
+            for (int i = p * 2; i <= 10000000; i += p)
+            {
+                prime[i] = 0;
+            }
+}
+
+
+.
