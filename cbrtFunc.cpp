e2783d5a55ef4cf636abdc3f26d428b4c2d8a263
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2022-08-01 07:59:52 +0530

    cube root function

---
+++ cbrtFunc.cpp
@@ -0,0 +1,20 @@
+
+
+int cbrt(int x)
+{
+    int lo = 1, hi = min(2000000ll, x);
+    while (hi - lo > 1)
+    {
+        int mid = (lo + hi) / 2;
+        if (mid * mid * mid < x)
+        {
+            lo = mid;
+        }
+        else
+            hi = mid;
+    }
+    if (hi * hi * hi <= x)
+        return hi;
+    else
+        return lo;
+}
