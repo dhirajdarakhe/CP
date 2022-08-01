e2d2a98de12e4c621e5be40ab58c5318ded96fa3
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2022-08-01 09:30:49 +0530

    bfs on undirected graph

---
+++ DSA/GRAPH/cycleInGRAPh/BFS_unDG.cpp
@@ -0,0 +1,21 @@
+const int N = 1e3 + 10;
+vector<int>graph[N];
+bool vis[N];
+
+bool bfs(int source) {
+    queue< pair<int, int> > q;
+    q.push({source, 0});
+    vis[source] = 1;
+    while (!q.empty()) {
+        int child = q.front().first;
+        int pare = q.front().second;
+        for (auto it : graph[child]) {
+            if (vis[it] == 1 && it != pare) return true;
+            if (!vis[it]) {
+                vis[it] = 1;
+                q.push({it, child});
+            }
+        }
+    }
+    return false;
+}
