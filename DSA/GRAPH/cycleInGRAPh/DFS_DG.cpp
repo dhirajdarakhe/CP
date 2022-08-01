0598da552060e4151228e8e7da720a0fef040b7b
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2022-08-01 09:44:49 +0530

    dfs on directed graph

--- DSA/GRAPH/cycleInGRAPh/DFS_DG.cpp
+++ DSA/GRAPH/cycleInGRAPh/DFS_DG.cpp
@@ -1,1 +1,26 @@
 
+const int N = 1e3 + 10;
+vector<int>graph[N];
+bool vis[N];
+bool mark[N]; // pathVis
+
+bool dfs(int source) {
+    vis[source] = 1;
+    mark[source] = 1;
+
+    for (auto child : graph[source]) {
+
+        if (vis[child] == 0) {
+            if (dfs(child) == true) {
+                return true;
+            }
+        }
+        if (vis[child] == 1 && mark[child] == 1) {
+            return true;
+        }
+    }
+
+    mark[source] = 0;
+    return false;
+
+}
