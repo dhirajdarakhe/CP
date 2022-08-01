96cbb63d7a4fd5e4e6fbdfacc4a3ef753dd26c9f
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2022-08-01 19:59:38 +0530

    recursion

--- DSA/GRAPH/bipertiteGraph/dfs.cpp
+++ DSA/GRAPH/bipertiteGraph/dfs.cpp
@@ -1,1 +1,25 @@
-x
+
+const int N = 1e5 + 7;
+vector<int> graph[N];
+bool col[N];
+int vis[N];
+bool DFSbipaterate(int source, bool color)
+{
+    col[source] = color;
+    vis[source] = 1;
+    for (auto child : graph[source])
+    {
+        if (vis[child] == 0)
+        {
+            if (DFSbipaterate(child, !color) == true)
+            {
+                return true;
+            }
+        }
+        if (vis[child] == 1 && col[child] == col[source])
+        {
+            return true;
+        }
+    }
+    return false;
+}
