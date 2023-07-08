08129913a1a9dfab6d50d53084e1853fcd92b103
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2023-07-08 12:54:46 +0530

    Create dijistra.cpp

---
+++ DSA/GRAPH/dijistra.cpp
@@ -0,0 +1,19 @@
+d.assign(n, INF);
+d[s] = 0;
+set<pair<int, int>> q;
+q.insert({0, s});
+while (!q.empty()) {
+    int v = q.begin()->second;
+    q.erase(q.begin());
+
+    for (auto edge : adj[v]) {
+        int u = edge.first;
+        int w = edge.second;
+
+        if (d[v] + w < d[u]) {
+            q.erase({d[u], u});
+            d[u] = d[v] + w;
+            q.insert({d[u], u});
+        }
+    }
+}
